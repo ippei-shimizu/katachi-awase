a739d44f10e17f83ffcd078cb540c7e76715aee0
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-03-29 22:13:47 +0900

    Add: LessonCategoriesテーブルの作成

---
+++ packages/api/src/db/migrations/0001_far_vargas.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "lesson_categories" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"slug" text NOT NULL,
+	"position" serial NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
--- packages/api/src/db/schema.ts
+++ packages/api/src/db/schema.ts
@@ -9,7 +9,19 @@ export const adminUsers = pgTable("admin_users", {
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
-export const schema = { adminUsers };
+export const lessonCategories = pgTable("lesson_categories", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  slug: text("slug").notNull(),
+  position: serial("position").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+
+export const schema = { adminUsers, lessonCategories };
 
 export type AdminUser = typeof adminUsers.$inferSelect;
 export type NewAdminUser = typeof adminUsers.$inferInsert;
+export type LessonCategory = typeof lessonCategories.$inferSelect;
+export type NewLessonCategory = typeof lessonCategories.$inferInsert;
